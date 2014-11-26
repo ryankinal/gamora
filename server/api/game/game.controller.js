d87dd5ec8788e69247cbfe47792bd8d55c968d37
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-26 10:23:33 -0500

    Initial yo build and a few endpoints

---
+++ server/api/game/game.controller.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _ = require('lodash');
+var Game = require('./game.model');
+
+// Get list of games
+exports.index = function(req, res) {
+  Game.find(function (err, games) {
+    if(err) { return handleError(res, err); }
+    return res.json(200, games);
+  });
+};
+
+// Get a single game
+exports.show = function(req, res) {
+  Game.findById(req.params.id, function (err, game) {
+    if(err) { return handleError(res, err); }
+    if(!game) { return res.send(404); }
+    return res.json(game);
+  });
+};
+
+// Creates a new game in the DB.
+exports.create = function(req, res) {
+  Game.create(req.body, function(err, game) {
+    if(err) { return handleError(res, err); }
+    return res.json(201, game);
+  });
+};
+
+// Updates an existing game in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Game.findById(req.params.id, function (err, game) {
+    if (err) { return handleError(res, err); }
+    if(!game) { return res.send(404); }
+    var updated = _.merge(game, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.json(200, game);
+    });
+  });
+};
+
+// Deletes a game from the DB.
+exports.destroy = function(req, res) {
+  Game.findById(req.params.id, function (err, game) {
+    if(err) { return handleError(res, err); }
+    if(!game) { return res.send(404); }
+    game.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.send(204);
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.send(500, err);
+}
---
+++ server/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+var errors = require('./components/errors');
+
+module.exports = function(app) {
+
+  // Insert routes below
+  app.use('/api/tags', require('./api/tag'));
+  app.use('/api/reviews', require('./api/review'));
+  app.use('/api/games', require('./api/game'));
+  app.use('/api/things', require('./api/thing'));
+  app.use('/api/users', require('./api/user'));
+
+  app.use('/auth', require('./auth'));
+  
+  // All undefined asset or api routes should return a 404
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+   .get(errors[404]);
+
+  // All other routes should redirect to the index.html
+  app.route('/*')
+    .get(function(req, res) {
+      res.sendfile(app.get('appPath') + '/index.html');
+    });
+};
