63527bd4d43700833d63f78aab3f65b043b01eb0
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-26 14:36:01 -0500

    Removed references to Thing model

--- server/routes.js
+++ server/routes.js
@@ -13,11 +13,10 @@ module.exports = function(app) {
   app.use('/api/tags', require('./api/tag'));
   app.use('/api/reviews', require('./api/review'));
   app.use('/api/games', require('./api/game'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
