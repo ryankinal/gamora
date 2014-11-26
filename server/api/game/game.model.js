6f328711495e032e1c47f28677a79766d7c35011
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-26 10:56:10 -0500

    Updated fields for Game model

--- server/api/game/game.model.js
+++ server/api/game/game.model.js
@@ -1,11 +1,29 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId;
 
 var GameSchema = new Schema({
-  name: String,
-  info: String,
+  title: {type: String, required: true},
+  publisher: String,
+  aliases: [String],
+  description: [{
+    text: String,
+    updated: Number, // UTC time
+    user: ObjectId
+  }],
+  rating: Number,
+  tags: [{
+    tag: ObjectId,
+    added: Number, // UTC time
+    user: ObjectId
+  }],
+  updated: [{
+    by: ObjectId,
+    on: Number // UTC time
+    fields: [String]
+  }],
   active: Boolean
 });
 
