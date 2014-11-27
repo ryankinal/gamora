b00394e1d54d9e95712e8511b170f2d03421df46
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-27 10:57:43 -0500

    Added ng service to access game API endpoint

---
+++ client/app/services/game/game.service.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('gamoraApp')
+  .factory('game', function (http, $q, _) {
+    var endpoint = '/api/games';
+
+    return {
+      get: function(filters) {
+        return http(endpoint, 'GET', filters);
+      },
+      getById: function(id) {
+        var url = endpoint + '/' + id;
+        return http(url, 'GET');
+      },
+      save: function(game) {
+        var url = endpoint,
+          method = 'POST';
+
+        if (game._id) {
+          url += '/' + id;
+          method = 'PUT';
+        }
+
+        return http(url, method, game);
+      },
+      destroy: function(id) {
+        if (!id) {
+          return $q.reject({error: 'ID is required'});
+        } else {
+          return http(url + '/' + id, 'DELETE');
+        }
+      }
+    };
+  });
