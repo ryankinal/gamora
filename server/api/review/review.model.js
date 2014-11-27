35adf98cfaadd6894ab7610cacc08b8212157b3f
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-27 09:47:13 -0500

    Added some more validation to review schema

--- server/api/review/review.model.js
+++ server/api/review/review.model.js
@@ -4,11 +4,15 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.Types.ObjectId;
 
+var isInt = function isInt(val) {
+  return typeof val === 'number' && Math.floor(val) === val;
+};
+
 var ReviewSchema = new Schema({
   title: String,
-  rating: {type: Number, min: 1, max: 5 },
-  difficulty: {type: Number, min: 1, max: 5 },
-  length: Number,
+  rating: {type: Number, min: 1, max: 5, validator: isInt },
+  difficulty: {type: Number, min: 1, max: 5, validator: isInt },
+  length: {type: Number, min: 0, validator: isInt },
   completed: {type: Number, default: 0, validator: function(val) {
     return val === -1 || val === 0 || val === 1;
   }}, // 0 = no, 1 = yes, -1 = not applicable
