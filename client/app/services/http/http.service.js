73eb3c5433148365a2180bcef4ec8b3e4a460817
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-27 18:18:42 -0500

    Corrected response bug in http service

--- client/app/services/http/http.service.js
+++ client/app/services/http/http.service.js
@@ -11,13 +11,14 @@ angular.module('gamoraApp')
     };
 
     return function(url, method, data) {
+      console.log(url, method, data);
       var deferred = $q.defer(),
         options = {
           method: method
         };
 
       if (method.toUpperCase() === 'GET') {
-        options.url = (url + (data) ? '?' + queryStringify(data) : '');
+        options.url = url + ((data) ? '?' + queryStringify(data) : '');
       } else {
         options.url = url;
 
@@ -26,8 +27,10 @@ angular.module('gamoraApp')
         }
       }
 
+      console.log(options);
+
       $http(options).success(function(response) {
-        deferred.resolve(response.data);
+        deferred.resolve(response);
       }).error(function(error) {
         deferred.reject(error);
       });
