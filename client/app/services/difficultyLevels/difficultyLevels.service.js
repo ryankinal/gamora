890a28e2b8db363e4b2320596aaad1e71465e480
Ryan Kinal <ryan.kinal@gmail.com>
2014-11-29 15:26:41 -0500

    Added difficulty levels service

--- client/app/services/difficultyLevels/difficultyLevels.service.js
+++ client/app/services/difficultyLevels/difficultyLevels.service.js
@@ -2,11 +2,12 @@
 
 angular.module('gamoraApp')
   .factory('difficultyLevels', function () {
-    return {
-      1: 'Really Easy',
-      2: 'Easy',
-      3: 'Challenging',
-      4: 'Hard',
-      5: 'Nightmarish'
-    };
+    return [
+      undefined,
+      'Really Easy',
+      'Easy',
+      'Challenging',
+      'Hard',
+      'Nightmarish'
+    ];
   });
